b5a4c68bb2c482f92d1fcaa39aa3ed31f8909334
nlamonaco86 <nlamonaco86@gmail.com>
2020-08-18 00:19:03 -0400

    HTML routes working

---
+++ routes/htmlR.js
@@ -0,0 +1,13 @@
+// dependencies 
+var path = require("path");
+
+module.exports = function(app) {
+// route to the index on load
+    app.get("/", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+// route to notes when you navigate there
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });  
+  };
---
+++ server.js
@@ -0,0 +1,18 @@
+//Dependencies
+var express = require("express");
+// Allows us to run multiple "servers" if needed, boilerplate
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+//set up for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//find the html routes function
+require("./routes/htmlR")(app);
+
+//run the server and log the port number to confirm it
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
